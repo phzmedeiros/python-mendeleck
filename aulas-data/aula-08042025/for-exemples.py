c727fda37dceb587a9a5d2a0087ff3379e0754cb
Pedro Henrique Medeiros dos Reis <pedrohenriquemreis@gmail.com>
2025-04-08 08:33:37 -0300

    update

---
+++ aulas-data/aula-08042025/for-exemples.py
@@ -0,0 +1,4 @@
+# for tem um conjunto de controle
+# for uma variavel "cont" in range(0,5)
+# PARA O CONTADOR DENTRO DO INTERVALO - NÃO PRECISO DE INTEGRADOR 
+# WHILE EU CONTROLO A CONDIÇÃO E CONSIGO VARIAR O INTERVALO
